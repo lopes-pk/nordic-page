b58fe3a6e504d2a8c844237af037ddc64847837c
lopes-pk <arthurlopes558@gmail.com>
2023-05-01 17:53:06 -0300

    salvando

---
+++ index.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+    
+  },
+  autoplay: {
+    delay: 2000,
+  },
+  
+}); 
+
+
+
+
